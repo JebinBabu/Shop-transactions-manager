9c859a47a87206392e36a60075a19fde109cf01d
Jebin <jebinbabuc@gmail.com>
2021-10-10 19:57:12 +0530

    initial

---
+++ app.js
@@ -0,0 +1,44 @@
+const express = require('express')
+const ch = require('child_process')
+const app = express();
+const port = 3000
+const loginController = require('./controllers/LoginController.js')
+const dashboardController = require('./controllers/DashboardController.js')
+const productsController = require('./controllers/ProductsController.js')
+const transactionsController = require('./controllers/TransactionsController.js')
+const registerController = require('./controllers/RegisterController.js')
+
+const mysql = require('mysql');
+
+const con = mysql.createConnection({
+  host: "127.0.0.1",
+  port:3307,
+  user: "root",
+  password: "root",
+  database: "shop"
+});
+
+con.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected!");
+});
+
+
+app.set('view engine','ejs');
+app.use(express.static('./public'));
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+loginController(app,con)
+dashboardController(app,con)
+productsController(app,con)
+transactionsController(app,con)
+registerController(app,con)
+
+
+app.listen(port,() => {
+
+  console.log('Started listening to port : ' + port);
+});
+
+ch.exec('start https://google.com',(err,stdout,stderr) => {})
--- readme.md
+++ readme.md
@@ -1,4 +1,6 @@
 Tharakan's shop management app.
 Simple node.js based app to maintain sales and stocks.
 
+Run " npm install " after downloading the code
+
 To know more, contact me : jebinbabuc@gmail.com
